1e7256b12784436adeb3543013ba60b5762290ca
Arturo Rivas <arturo.rivas@icloud.com>
2016-04-07 23:00:51 +0200

    Versión final

--- Peticiones a OpenLibrary/ViewController.swift
+++ Peticiones a OpenLibrary/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pulsoBuscar(sender: AnyObject) {
+        let textField = sender as! UITextField
+        
+        let url = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textField.text!)
+        
+        do {
+            let respuesta = try String(contentsOfURL: url!, encoding: NSUTF8StringEncoding)
+            
+            if respuesta != "{}" {
+                textView.text = respuesta
+            } else {
+                textView.text = "El ISBN proporcionado no corresponde a ningún libro registrado en Open Library"
+            }
+            
+        } catch {
+            let alert = UIAlertController(title: "Error", message: "Hay problemas con la conexión a Internet. Inténtelo de nuevo más tarde.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
